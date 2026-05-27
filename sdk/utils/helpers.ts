b726bcfc34726837ac4a884f5590ef2778aa33d8
hjunkim <blue45f@gmail.com>
2026-05-27 10:28:24 +0900

    refactor: centralize sdk env access

--- sdk/utils/helpers.ts
+++ sdk/utils/helpers.ts
@@ -1,3 +1,5 @@
+import { readSdkEnv } from "./env";
+
 declare global {
   interface Window {
     JavaScriptInterface?: {
@@ -10,12 +12,9 @@ declare global {
  * 방 주소를 변환하는 함수
  */
 export const convertLink = (room: string, recordId: number | null) => {
-  // NOTE: 개발 HMR 활용될 때 import.meta.env 가 없으므로 이를 대응하기 위한 코드
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const host = import.meta.env?.VITE_INTERNAL_HOST || "http://localhost:3000";
+  const host = readSdkEnv("VITE_INTERNAL_HOST", "http://localhost:3000");
   const record = recordId ? `&recordMode=true&recordId=${recordId}` : "";
-  const wsHost = host?.replace(/^https?:\/\/(.+)$/, "$1");
+  const wsHost = host.replace(/^https?:\/\/(.+)$/, "$1");
 
   const protocol = host.startsWith("https") ? "wss" : "ws";
   // DevTools frontend에서 protocol을 추가하므로 host만 전달
